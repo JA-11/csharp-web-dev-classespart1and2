456b465ebd3094be261a132643608dac21e37c77
Jordan <jordan_anthony11@aol.com>
2022-08-17 16:27:37 -0500

    Worked on Chapter 4 exercises

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        private string subject;
+        private string teacherName;
+        private List<Student> enrolledStudents;
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,11 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+            Student jordan = new Student("Jordan", 1234, 1, 4.0);
+
+            Console.WriteLine($"The Student class works! {jordan.Name} is a new student!");
+
         }
     }
 }
